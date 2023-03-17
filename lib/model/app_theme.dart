7bee54a41e114706813bbd5d1c3806eb2ed437fd
Utkarsh Tiwari <utkarsh02t@gmail.com>
2023-03-17 15:37:12 +0530

    Build on master version of flutter

--- lib/model/app_theme.dart
+++ lib/model/app_theme.dart
@@ -7,6 +7,7 @@ class ThemeProvider extends ChangeNotifier {
       : ThemeMode.light; //ThemeMode.light;
 
   bool get isDarkMode => themeMode == ThemeMode.dark;
+
   void toggleTheme(bool isOn) {
     themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
     AppSecurePreferencesStorage.setIsThemeDark(isOn);
@@ -16,38 +17,34 @@ class ThemeProvider extends ChangeNotifier {
 
 class AppThemes {
   static final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
     primaryColor: Colors.black,
     brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    accentColor: Colors.white,
-    accentIconTheme: IconThemeData(color: Colors.black),
     dividerColor: Colors.black12,
     appBarTheme: AppBarTheme(
       //color: Color.fromRGBO(0, 265, 45, 50)
       color: Colors.blueGrey.shade800,
     ),
-    colorScheme: ColorScheme.dark(primary: Colors.blue),
     scaffoldBackgroundColor: Colors.blueGrey.shade900,
     floatingActionButtonTheme:
         FloatingActionButtonThemeData(backgroundColor: Colors.grey),
+    colorScheme: ColorScheme.dark(primary: Colors.blue)
+        .copyWith(
+            primaryContainer: Colors.grey, background: const Color(0xFF212121))
+        .copyWith(secondary: Colors.white),
   );
 
   static final lightTheme = ThemeData(
-    primarySwatch: Colors.grey,
     primaryColor: Colors.white,
     brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-    accentColor: Colors.black,
-    accentIconTheme: IconThemeData(color: Colors.white),
     dividerColor: Colors.white54,
-    colorScheme: ColorScheme.light(
-        primary: const Color(0xff303F9F),
-        primaryVariant: const Color(0xff3F51B5)),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: Colors.blueGrey.shade500),
     appBarTheme: AppBarTheme(
       color: Colors.blueGrey.shade500,
     ),
+    colorScheme: ColorScheme.light(primary: const Color(0xff303F9F))
+        .copyWith(
+            primaryContainer: Colors.grey, background: const Color(0xFFE5E5E5))
+        .copyWith(secondary: Colors.black),
   );
 }
